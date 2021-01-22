fa87446cf074688db040d5b82eedca1691ca8838
Derin Durak <durakderin@gmail.com>
2021-01-22 19:46:55 +0300

    Add files via upload

---
+++ js/calparbox.js
@@ -0,0 +1,34 @@
+var elem = document.getElementById("liveimg");
+let fullscreen = false;
+
+/* View in fullscreen */
+function openFullscreen() {
+    if (fullscreen) {
+        closeFullscreen();
+    } else {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) {
+            /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) {
+            /* IE11 */
+            elem.msRequestFullscreen();
+        }
+        fullscreen = true;
+    }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+        /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        /* IE11 */
+        document.msExitFullscreen();
+    }
+    fullscreen = false;
+}
